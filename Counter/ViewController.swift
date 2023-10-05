1a1b8582456f43a648637f0aa603b0ed671cc2b6
LightKrite <egorpartenko@icloud.com>
2023-10-05 17:13:57 +0500

    Добавление кнопки и счетчика. Фиксирование логики взаимосвязи. Задан концепт дизайна

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterText: UILabel!
+    private var counterNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterText.text = "0"
+        counterButton.tintColor = .orange
+        
     }
 
-
+    @IBAction func counterButtonTap(_ sender: Any) {
+        counterNumber += 1
+        counterText.text = "Значение счетчика: \(counterNumber)"
+    }
+    
 }
 
